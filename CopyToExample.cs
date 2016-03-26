b830838bc395058aa13d334d6d8addd3bc20b865
Vladimir nettsundere Kiselev <nettsundere@gmail.com>
2016-03-27 01:36:37 +0300

    Add bounds checking examples

---
+++ CopyToExample.cs
@@ -0,0 +1,311 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Collections.Specialized;
+
+namespace CollectionsCopyTo
+{
+    public static class CopyToExample
+    {
+        public static void ArrayListCopyTo(bool greaterIndex = false) {
+            var arrayList = new ArrayList();
+            var buffer = new object[1];
+            if(greaterIndex)
+            {
+                arrayList.CopyTo(buffer, 2);
+            }
+            else
+            {
+                arrayList.CopyTo(buffer, 1);
+            }
+        }
+        
+        public static void BitArrayCopyTo(bool greaterIndex = false) {
+            var bitArray = new BitArray(20, false);
+            var bitArrayAsCollection = bitArray as ICollection;
+            var buffer = new bool[1];
+            if(greaterIndex)
+            {
+                bitArrayAsCollection.CopyTo(buffer, 2);
+            }
+            else
+            {
+                bitArrayAsCollection.CopyTo(buffer, 1);
+            }
+        }
+        
+        public static void ConcurrentBagCopyTo(bool greaterIndex = false)
+        {
+            var concurrentBag = new ConcurrentBag<string>();
+            var buffer = new string[1];
+            if(greaterIndex)
+            {
+                concurrentBag.CopyTo(buffer, 2);
+            }
+            else
+            {
+                concurrentBag.CopyTo(buffer, 1);
+            }
+        }
+        
+        public static void ConcurrentDictionaryCopyTo(bool greaterIndex = false)
+        {
+            var dictionary = new ConcurrentDictionary<string, string>();
+            var buffer = new KeyValuePair<string, string>[1];
+            var dictionaryAsCollection = dictionary as ICollection;
+            if(greaterIndex)
+            {
+                dictionaryAsCollection.CopyTo(buffer, 2);
+            }
+            else
+            {
+                dictionaryAsCollection.CopyTo(buffer, 1);
+            }
+        }
+        
+        public static void DictionaryCopyTo(bool greaterIndex = false)
+        {
+            var dictionary = new Dictionary<string, string>();
+            var buffer = new KeyValuePair<string, string>[1];
+            var dictionaryAsCollection = dictionary as ICollection;
+            if(greaterIndex)
+            {
+                dictionaryAsCollection.CopyTo(buffer, 2);
+            }
+            else
+            {
+                dictionaryAsCollection.CopyTo(buffer, 1);
+            }
+        }
+        
+        public static void HashSetCopyTo(bool greaterIndex = false)
+        {
+            var hashSet = new HashSet<string>();
+            var buffer = new string[1];
+            if(greaterIndex)
+            {
+                hashSet.CopyTo(buffer, 2);
+            }
+            else
+            {
+                hashSet.CopyTo(buffer, 1);
+            }
+        }
+        
+        public static void HashtableCopyTo(bool greaterIndex = false)
+        {
+            var hashTable = new Hashtable();
+            var buffer = new KeyValuePair<string, string>[1];
+            var hashtableAsCollection = hashTable as ICollection;
+            if(greaterIndex)
+            {
+                hashtableAsCollection.CopyTo(buffer, 2);
+            }
+            else
+            {
+                hashtableAsCollection.CopyTo(buffer, 1);
+            }
+        }
+        
+        public static void HybridDictionaryCopyTo(bool greaterIndex = false)
+        {
+            var hybridDictionary = new HybridDictionary();
+            var buffer = new KeyValuePair<string, string>[1];
+            var hybridDictionaryAsCollection = hybridDictionary as ICollection;
+            if(greaterIndex)
+            {
+                hybridDictionaryAsCollection.CopyTo(buffer, 2);
+            }
+            else
+            {
+                hybridDictionaryAsCollection.CopyTo(buffer, 1);
+            }
+        }
+        
+        public static void LinkedListCopyTo(bool greaterIndex = false)
+        {
+            var linkedList = new LinkedList<string>();
+            var buffer = new string[1];
+            if(greaterIndex)
+            {
+                linkedList.CopyTo(buffer, 2);
+            }
+            else
+            {
+                linkedList.CopyTo(buffer, 1);
+            }
+        }
+        
+        public static void ListDictionaryCopyTo(bool greaterIndex = false)
+        {
+            var listDictionary = new ListDictionary();
+            var buffer = new object[1];
+            if(greaterIndex)
+            {
+                listDictionary.CopyTo(buffer, 2);
+            }
+            else
+            {
+                listDictionary.CopyTo(buffer, 1);
+            }
+        }
+        
+        public static void NameValueCollectionCopyTo(bool greaterIndex = false)
+        {
+            var nameValueCollection = new NameValueCollection();
+            var buffer = new string[1];
+            if(greaterIndex)
+            {
+                nameValueCollection.CopyTo(buffer, 2);
+            }
+            else
+            {
+                nameValueCollection.CopyTo(buffer, 1);
+            }
+        }
+        
+        public static void QueueGenericCopyTo(bool greaterIndex = false)
+        {
+            var queue = new Queue<string>();
+            var buffer = new string[1];
+            if(greaterIndex)
+            {
+                queue.CopyTo(buffer, 2);
+            }
+            else
+            {
+                queue.CopyTo(buffer, 1);
+            }
+        }
+
+        public static void QueueCopyTo(bool greaterIndex = false)
+        {
+            var queue = new Queue();
+            var buffer = new object[1];
+            if(greaterIndex)
+            {
+                queue.CopyTo(buffer, 2);
+            }
+            else
+            {
+                queue.CopyTo(buffer, 1);
+            }
+        }
+
+        public static void ReadOnlyCollectionCopyTo(bool greaterIndex = false)
+        {
+            var readonlyCollection = new List<string>().AsReadOnly();
+            var buffer = new string[1];
+            if(greaterIndex)
+            {
+                readonlyCollection.CopyTo(buffer, 2);
+            }
+            else
+            {
+                readonlyCollection.CopyTo(buffer, 1);
+            }
+        }
+
+        public static void SortedDictionaryCopyTo(bool greaterIndex = false)
+        {
+            var sortedDictioanary = new SortedDictionary<string, string>();
+            var buffer = new KeyValuePair<string, string>[1];
+            var sortedDictionaryAsCollection = sortedDictioanary as ICollection;
+            if(greaterIndex)
+            {
+                sortedDictionaryAsCollection.CopyTo(buffer, 2);
+            }
+            else
+            {
+                sortedDictionaryAsCollection.CopyTo(buffer, 1);
+            }
+        }
+        
+        public static void SortedListGenericCopyTo(bool greaterIndex = false)
+        {
+            var sortedList = new SortedList<string, string>();
+            var buffer = new KeyValuePair<string, string>[1];
+            var sortedListAsCollection = sortedList as ICollection;
+            if(greaterIndex)
+            {
+                sortedListAsCollection.CopyTo(buffer, 2);
+            }
+            else
+            {
+                sortedListAsCollection.CopyTo(buffer, 1);
+            }
+        }
+
+        public static void SortedListCopyTo(bool greaterIndex = false)
+        {
+            var sortedList = new SortedList();
+            var buffer = new KeyValuePair<object, object>[1];
+            var sortedListAsCollection = sortedList as ICollection;
+            if(greaterIndex)
+            {
+                sortedListAsCollection.CopyTo(buffer, 2);
+            }
+            else
+            {
+                sortedListAsCollection.CopyTo(buffer, 1);
+            }
+        }
+
+        public static void SortedSetCopyTo(bool greaterIndex = false)
+        {
+            var sortedSet = new SortedSet<string>();
+            var buffer = new string[1];
+            if(greaterIndex)
+            {
+                sortedSet.CopyTo(buffer, 2);
+            }
+            else
+            {
+                sortedSet.CopyTo(buffer, 1);
+            }
+        }
+        
+        public static void StackGenericCopyTo(bool greaterIndex = false)
+        {
+            var stack = new Stack<string>();
+            var buffer = new string[1];
+            if(greaterIndex)
+            {
+                stack.CopyTo(buffer, 2);
+            }
+            else
+            {
+                stack.CopyTo(buffer, 1);
+            }
+        }
+
+        public static void StackCopyTo(bool greaterIndex = false)
+        {
+            var stack = new Stack();
+            var buffer = new object[1];
+            if(greaterIndex)
+            {
+                stack.CopyTo(buffer, 2);
+            }
+            else
+            {
+                stack.CopyTo(buffer, 1);
+            }
+        }
+
+        public static void StringDictionaryCopyTo(bool greaterIndex = false)
+        {
+            var stringDictionary = new StringDictionary();
+            var buffer = new string[1];
+            if(greaterIndex)
+            {
+                stringDictionary.CopyTo(buffer, 2);
+            }
+            else
+            {
+                stringDictionary.CopyTo(buffer, 1);
+            }
+        }
+    }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace CollectionsCopyTo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {   
+            Console.WriteLine("Trying to copy to buffer having size of 1 starting with index=1");
+            TestCollections(false);
+            
+            Console.WriteLine("Trying to copy to buffer having size of 1 starting with index=2");
+            TestCollections(true);
+        }
+        
+        public static void TestCollections(bool useNextNonExistentIndex = false)
+        {
+            FormatExceptionDetails("ArrayList", () => CopyToExample.ArrayListCopyTo(useNextNonExistentIndex));
+            FormatExceptionDetails("BitArray", () =>CopyToExample.BitArrayCopyTo(useNextNonExistentIndex));
+            FormatExceptionDetails("ConcurrentBag", () =>CopyToExample.ConcurrentBagCopyTo(useNextNonExistentIndex));
+            FormatExceptionDetails("ConcurrentDictionary", () =>CopyToExample.ConcurrentDictionaryCopyTo(useNextNonExistentIndex));
+            FormatExceptionDetails("Dictionary", () =>CopyToExample.DictionaryCopyTo(useNextNonExistentIndex));
+            FormatExceptionDetails("HashSet", () =>CopyToExample.HashSetCopyTo(useNextNonExistentIndex));
+            FormatExceptionDetails("Hashtable", () =>CopyToExample.HashtableCopyTo(useNextNonExistentIndex));
+            FormatExceptionDetails("HybridDictionary", () =>CopyToExample.HybridDictionaryCopyTo(useNextNonExistentIndex));
+            FormatExceptionDetails("LinkedList", () =>CopyToExample.LinkedListCopyTo(useNextNonExistentIndex));
+            FormatExceptionDetails("ListDictionary", () =>CopyToExample.ListDictionaryCopyTo(useNextNonExistentIndex));
+            FormatExceptionDetails("NameValueCollection", () =>CopyToExample.NameValueCollectionCopyTo(useNextNonExistentIndex));
+            FormatExceptionDetails("Queue", () =>CopyToExample.QueueCopyTo(useNextNonExistentIndex));
+            FormatExceptionDetails("Queue (Generic)", () =>CopyToExample.QueueGenericCopyTo(useNextNonExistentIndex));
+            FormatExceptionDetails("Readonly collection", () =>CopyToExample.ReadOnlyCollectionCopyTo(useNextNonExistentIndex));
+            FormatExceptionDetails("SortedDictionary", () => CopyToExample.SortedDictionaryCopyTo(useNextNonExistentIndex));
+            FormatExceptionDetails("SortedList", () =>CopyToExample.SortedListCopyTo(useNextNonExistentIndex));
+            FormatExceptionDetails("SortedList (Generic)", () => CopyToExample.SortedListGenericCopyTo(useNextNonExistentIndex));
+            FormatExceptionDetails("Sorted set", () => CopyToExample.SortedSetCopyTo(useNextNonExistentIndex));
+            FormatExceptionDetails("Stack", () => CopyToExample.StackCopyTo(useNextNonExistentIndex));
+            FormatExceptionDetails("Stack (Generic)", () =>CopyToExample.StackGenericCopyTo(useNextNonExistentIndex));
+            FormatExceptionDetails("StringDictionary", () => CopyToExample.StringDictionaryCopyTo(useNextNonExistentIndex));
+        }
+        
+        public static void FormatExceptionDetails(string header, Action exampleAction)
+        {
+            Console.WriteLine(new String('-', 50));
+            Console.WriteLine(header);
+            try 
+            {
+                exampleAction();
+                Console.WriteLine("No exceptions");
+                Console.WriteLine();
+            }
+            catch(Exception exception)
+            {
+
+                Console.WriteLine(exception);
+                Console.WriteLine();
+            }
+        }
+    }
+}
